36f89b896dbac67440557d7534f0622a95a53386
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-02-13 21:01:03 -0700

    8. El método subscribe()

--- src/main/java/com/rodarte/springreactor/SpringReactorApplication.java
+++ src/main/java/com/rodarte/springreactor/SpringReactorApplication.java
@@ -1,5 +1,7 @@
 package com.rodarte.springreactor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +9,8 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringReactorApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringReactorApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactorApplication.class, args);
@@ -21,11 +25,23 @@ public class SpringReactorApplication implements CommandLineRunner {
 		// doOnNext: el equivalente a tap en rxjs
 		Flux<String> nombres =
 			Flux
-				.just("Andres", "Pedro", "Diego", "Juan")
-				.doOnNext(System.out::println);
+				.just("Andres", "Pedro", "", "Diego", "Juan")
+				.doOnNext(nombre -> {
 
-		// suscripcion
-		nombres.subscribe();
+					if (nombre.isEmpty()) {
+						throw new RuntimeException("Nombres no pueden ser vacios");
+					}
+
+					System.out.println(nombre);
+
+				});
+
+		// suscripcion; metodos next y error
+		// errores interrumpen el flujo y matan la suscripcion
+		nombres.subscribe(
+			logger::info,
+			err -> logger.error(err.getMessage())
+		);
 
 	}
 
